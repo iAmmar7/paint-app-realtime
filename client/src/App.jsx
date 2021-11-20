8ea8a058cb6edf8fd37e78f08d7dfd7869ee095c
Ammar Ansari <iammaransari@gmail.com>
2021-11-20 21:33:09 +0500

    server URl bug resolved for socket connection

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,8 +4,12 @@ import socketClient from 'socket.io-client';
 import Canvas from './components/Canvas';
 
 const URL =
-  process.env.NODE_ENV === 'production' ? process.env.REACT_APP_PROD_SERVER_URL : process.env.REACT_APP_DEV_SERVER_URL;
+  process.env.REACT_APP_NODE_ENV === 'production'
+    ? process.env.REACT_APP_PROD_SERVER_URL
+    : process.env.REACT_APP_DEV_SERVER_URL;
 const socket = socketClient(URL);
+
+console.log('URL', URL);
 
 const App = () => {
   const [elements, setElements] = useState({ currentState: [], previousState: [] });
