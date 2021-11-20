7bc9aa55a966d35c72a12da584c1de62e9b7340a
Ammar Ansari <iammaransari@gmail.com>
2021-11-21 00:42:41 +0500

    socket issue resolved for cluster mode

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,13 @@
 module.exports = {
   apps: [
     {
-      name: 'realtime-draw',
+      name: 'paint-app-realtime',
       script: 'index.js',
       instances: 2,
       exec_mode: 'cluster',
       env: {
         NODE_ENV: 'development',
+        PORT: 5000,
       },
       env_production: {
         NODE_ENV: 'production',
--- index.js
+++ index.js
@@ -2,16 +2,33 @@ require('dotenv').config();
 const process = require('process');
 const app = require('express')();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http, {
+const redisAdapter = require('socket.io-redis');
+// const io = require('socket.io')(http, {
+//   cors: {
+//     origin: '*',
+//   },
+//   transports: ['websocket'],
+// });
+
+global.io = require('socket.io')(http, {
   cors: {
     origin: '*',
+    methods: ['GET', 'POST'],
+    credentials: false,
   },
+  transports: ['websocket', 'polling'],
+  allowEIO3: true,
 });
 
-io.on('connection', (socket) => {
+const PORT = process.env.PORT || 5000;
+
+io.adapter(redisAdapter({ host: 'localhost', port: 6379 }));
+
+global.io.on('connection', (socket) => {
   console.log(`User connected on process ${process.pid}`);
 
   socket.on('canvas-data', (data) => {
+    console.log('sending data', process.pid);
     socket.broadcast.emit('canvas-data', { data: data.data, id: process.pid });
   });
 
@@ -19,8 +36,6 @@ io.on('connection', (socket) => {
     console.log(`User disconnected from process ${process.pid}`);
   });
 });
-
-const PORT = process.env.PORT || 5000;
 
 http.listen(PORT, () => {
   console.log(`Listening all instances on port: ${PORT}`);
