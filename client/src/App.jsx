66ecac287fc34c754db4c88954e7010787225479
Ammar Ansari <iammaransari@gmail.com>
2022-01-06 01:37:06 +0500

    heroku domain updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ const URL =
     ? process.env.REACT_APP_PROD_SERVER_URL
     : process.env.REACT_APP_DEV_SERVER_URL;
 
-const socket = socketClient(URL || 'https://fierce-fortress-85118.herokuapp.com/', {
+const socket = socketClient(URL || 'https://api-paint-app-realtime.herokuapp.com/', {
   transports: ['websocket', 'polling'],
 });
 
